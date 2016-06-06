2c4832b50125fc535f6bda0990c60f027fef4419
Thiago Tobias <thiagotobias@gmail.com>
2016-06-06 07:39:34 -0300

    PHP Fundatio validacao campo vazios

--- contato.php
+++ contato.php
@@ -3,8 +3,20 @@
 <?php
     require_once("head.php");
 
-    var_dump($_POST);
-var_dump($_GET);
+    if($_POST['btn_enviar'] === 'btn_enviar'){
+
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $assunto = $_POST['assunto'];
+        $mensagem = $_POST['mensagem'];
+        if(empty($nome) AND empty($email) AND empty($assunto) AND empty($mensagem)){
+            $msg_erro = "Os campos estão fazios, favor preencher";
+            unset($nome);
+            unset($email);
+            unset($assunto);
+            unset($mensagem);
+        }
+    }
 ?>
 <body>
 <?php
@@ -12,28 +24,45 @@ var_dump($_GET);
     require_once("menu.php");
 ?>
 <div class="container">
-    <form role="form" name="frm_contato" method="POST" action="<?=$PHP_SELF?>">
+    <?php
+    if($_POST['btn_enviar'] === 'btn_enviar' AND empty($msg_erro)){?>
+
+        <div class="alert alert-success" role="alert">
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            Dados enviados com sucesso, abaixo seguem os dados que você enviou
+        </div>
+    <?php
+    }
+    if(!empty($msg_erro)){?>
+        <div class="alert alert-danger" role="alert">
+            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+            <?=$msg_erro?>
+        </div>
+    <?php
+    }
+    ?>
+    <form role="form" name="frm_contato" method="POST" action="contato.php">
         <div class="form-group">
             <label for="nome">Nome:</label>
-            <input type="text" class="form-control" id="nome">
+            <input type="text" class="form-control" id="nome" name="nome" value="<?=$nome?>">
         </div>
         <div class="form-group">
             <label for="email">Email address:</label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" class="form-control" id="email" name="email" value="<?=$email?>">
         </div>
         <div class="form-group">
             <label for="assunto">Assunto:</label>
-            <input type="assunto" class="form-control" id="assunto">
+            <input type="assunto" class="form-control" id="assunto" name="assunto" value="<?=$assunto?>">
         </div>
         <div class="form-group">
             <label for="mensagem">Mensagem:</label>
-            <textarea class="form-control" rows="3" id="mensagem"></textarea>
+            <textarea class="form-control" rows="3" id="mensagem" name="mensagem"><?=$mensagem?></textarea>
         </div>
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" class="btn btn-default" name="btn_enviar" value="btn_enviar" >Enviar</button>
     </form>
 </div>
 <?php
-require_once("rodape.php");
+    require_once("rodape.php");
 ?>
 </body>
 </html>
